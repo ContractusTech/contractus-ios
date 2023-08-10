6aae11689f961f18bd42b0899b25aca95ad08410
Simon Hudishkin <saman1303@gmail.com>
2023-08-10 16:42:27 +0500

    Fixed path

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,6 +1,6 @@
 if [[ -z "${GOOGLE_SERVICE_INFO}" ]]; then
   	echo "No Google service"
 else
-	cat $GOOGLE_SERVICE_INFO > /Contractus/Resourses/GoogleService-Info.plist
+	cat $GOOGLE_SERVICE_INFO > /Contractus/Resources/GoogleService-Info.plist
   	echo "Google service copied successfully"
 fi
